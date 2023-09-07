0ca00ca2af057f1bce4d265e14c4d01cb0df2507
Kishor Kumar Naik P <kishor88611@gmail.com>
2023-09-07 16:17:15 +0530

    text preprocessing

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,45 +1,40 @@
+
 import nltk
 
-
-paragraph = """I have three visions for India. In 3000 years of our history, people from all over 
+paragraph =  """I have three visions for India. In 3000 years of our history, people from all over 
                the world have come and invaded us, captured our lands, conquered our minds. 
                From Alexander onwards, the Greeks, the Turks, the Moguls, the Portuguese, the British,
                the French, the Dutch, all of them came and looted us, took over what was ours. 
                Yet we have not done this to any other nation. We have not conquered anyone. 
                We have not grabbed their land, their culture, 
-               their history and tried to enforce our way of life on them"""
+               their history and tried to enforce our way of life on them."""
+
+
 
 import re
-from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 
-wordnet = WordNetLemmatizer()
-
+ps = PorterStemmer()
+wordnet=WordNetLemmatizer()
 sentences = nltk.sent_tokenize(paragraph)
-corpus=[]
+corpus = []
 
 for i in range(len(sentences)):
-    # Remove non-alphabetic characters and convert to lowercase
     review = re.sub('[^a-zA-Z]', ' ', sentences[i])
     review = review.lower()
-    words = review.split()
-    # print(words)
-    review = [wordnet.lemmatize(word) for word in words if word not in set(stopwords.words("english"))]
+    # print(review)
+    review = review.split()
+    # print(review)
+    review = [wordnet.lemmatize(word) for word in review if word not in set(stopwords.words('english'))]
     print(review)
     review = ' '.join(review)
-    # print(review)
+    print(review)
     corpus.append(review)
 
-# Creating the Bag of Words model
-from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer(max_features = 1500)
+# Creating the TF-IDF model
+from sklearn.feature_extraction.text import TfidfVectorizer
+cv = TfidfVectorizer()
 X = cv.fit_transform(corpus).toarray()
-
 print(X)
-
-
-
-
-
-
-
